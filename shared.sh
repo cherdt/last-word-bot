a484f94f99bb068b8f56ff3dc8bebf1e5132778b
Chris Herdt <cherdt@gmail.com>
2017-11-25 13:33:59 -0600

    Add SOCIAL mode, fix error in follow_users (#9)

--- shared.sh
+++ shared.sh
@@ -14,6 +14,14 @@ is_enabled () {
     return 0
 }
 
+is_social () {
+    if [ -e $MYPATH/.social ]
+    then
+        return 0
+    fi
+    return 1
+}
+
 send_dm_reply () {
     twidge -c $MYPATH/$CONFIG dmsend $SENDER "$1"
 }
@@ -24,4 +32,12 @@ send_off_confirmation () {
 
 send_on_confirmation () {
     send_dm_reply "I am now ON. An authorized user can turn me off by sending the OFF command"
+}
+
+send_social_confirmation () {
+    send_dm_reply "I am now in SOCIAL mode and will respond to tweets DM'd from any follower. Try UNSOCIAL to change modes"
+}
+
+send_unsocial_confirmation () {
+    send_dm_reply "I am now in UNSOCIAL mode and will respond to tweets DM'd from authorized users only. Try SOCIAL to change modes"
 }
